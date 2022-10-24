64484c334ffc73534affc70ffcc006c0f1df749d
xxxpegaxxx <101995184+xxxpegaxxx@users.noreply.github.com>
2022-10-23 20:45:17 -0400

    Add files via upload

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -1,1 +1,18 @@
+# Resource Group/Location
 
+variable "application_type" {}
+variable "resource_type" {}
+variable "location" {}
+variable "resource_group" {}
+variable "subnet_id" {}
+variable "public_ip_address_id" {}
+
+variable "admin_username" {
+   description = "User name to use as the admin account on the VMs"
+   default     = "adminuser"
+}
+
+variable "admin_password" {
+   description  = "Default password for admin account"
+   default      = "Smooth$ky461"
+}
--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,27 +1,29 @@
-resource "azurerm_network_interface" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
+resource "azurerm_network_interface" "test" {
+  name                = "${var.application_type}-${var.resource_type}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = ""
+    subnet_id                     = "${var.subnet_id}"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = ""
+    public_ip_address_id          = "${var.public_ip_address_id}"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
+resource "azurerm_linux_virtual_machine" "test" {
+  name                = "${var.application_type}-${var.resource_type}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group}"
   size                = "Standard_DS2_v2"
-  admin_username      = ""
-  network_interface_ids = []
-  admin_ssh_key {
-    username   = ""
-    public_key = "file("~/.ssh/id_rsa.pub")"
-  }
+  admin_username      = "${var.admin_username}"
+  admin_password      = "${var.admin_password}"
+  disable_password_authentication = false
+  network_interface_ids = [azurerm_network_interface.test.id]
+  #admin_ssh_key {
+  #  username   = "adminuser"
+  #  public_key = "file("~/.ssh/id_rsa.pub")"
+  #}
   os_disk {
     caching           = "ReadWrite"
     storage_account_type = "Standard_LRS"
