ae563d64d4fc26d1234b94cc1f690abeed6cb908
xxxpegaxxx <101995184+xxxpegaxxx@users.noreply.github.com>
2022-10-23 20:45:58 -0400

    Add files via upload

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -7,10 +7,10 @@ provider "azurerm" {
 }
 terraform {
   backend "azurerm" {
-    storage_account_name = ""
-    container_name       = ""
-    key                  = ""
-    access_key           = ""
+    storage_account_name = "tfstatecjv"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    access_key           = "o06sEfAS2tC3/Pt9aFu+IfoL4gO4ZOewZoGYvBIjshxFucBCL6hzYIUPmR9Tpg/7L5R+knCKKk+D+AStzs58+w=="
   }
 }
 module "resource_group" {
@@ -51,4 +51,13 @@ module "publicip" {
   application_type = "${var.application_type}"
   resource_type    = "publicip"
   resource_group   = "${module.resource_group.resource_group_name}"
+}
+module "vm" {
+source           = "../../modules/vm"
+location         = "${var.location}"
+application_type = "${var.application_type}"
+resource_type    = "vm"
+resource_group   = "${module.resource_group.resource_group_name}"
+subnet_id         = "${module.network.subnet_id_test}"
+public_ip_address_id = "${module.publicip.public_ip_address_id}"
 }
--- terraform/environments/test/terraform.tfvars
+++ terraform/environments/test/terraform.tfvars
@@ -1,15 +1,15 @@
 # Azure subscription vars
-subscription_id = ""
-client_id = ""
-client_secret = ""
-tenant_id = ""
+subscription_id = "4a50042c-c104-43f9-bd2e-6d6c5e605046"
+client_id = "022a0b37-2f0c-434a-8c13-6ba611edee5d"
+client_secret = "dtS8Q~Re1Saezz3AlYQf4kfMUXxQ~b~9PedawdAc"
+tenant_id = "075321b7-c668-42fa-a1cc-71c01272ee64"
 
 # Resource Group/Location
-location = ""
-resource_group_name = ""
-application_type = ""
+location = "East US"
+resource_group_name = "Azuredevops"
+application_type = "myApplication"
 
 # Network
 virtual_network_name = ""
 address_space = ["10.5.0.0/16"]
-address_prefix_test = "10.5.1.0/24"
+address_prefix_test = ["10.5.1.0/24"]
